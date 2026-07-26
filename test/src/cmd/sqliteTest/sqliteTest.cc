a5749db70ac035958c55fc21dec68e5b196e82eb
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-07-26 22:53:10 +0900

    テストの見直し

--- test/src/cmd/sqliteTest/sqliteTest.cc
+++ test/src/cmd/sqliteTest/sqliteTest.cc
@@ -23,21 +23,24 @@ TEST_F(sqliteTest, exec_create_insert_select_roundtrip)
     // Act_2
     int create_rc = sqlite3_exec(db, "CREATE TABLE t (id INTEGER, name TEXT)", nullptr, nullptr,
                                  nullptr); // [手順] - CREATE TABLE 文を sqlite3_exec で実行する。
-    int insert_rc = sqlite3_exec(db, "INSERT INTO t (id, name) VALUES (1, 'sqlite')", nullptr, nullptr,
-                                 nullptr); // [手順] - INSERT 文を sqlite3_exec で実行する。
-
     // Assert_2
     ASSERT_EQ(SQLITE_OK,
               create_rc); // [確認_正常系] - CREATE TABLE 実行時の sqlite3_exec の戻り値が SQLITE_OK であること。
-    ASSERT_EQ(SQLITE_OK, insert_rc); // [確認_正常系] - INSERT 実行時の sqlite3_exec の戻り値が SQLITE_OK であること。
 
     // Act_3
+    int insert_rc = sqlite3_exec(db, "INSERT INTO t (id, name) VALUES (1, 'sqlite')", nullptr, nullptr,
+                                 nullptr); // [手順] - INSERT 文を sqlite3_exec で実行する。
+
+    // Assert_3
+    ASSERT_EQ(SQLITE_OK, insert_rc); // [確認_正常系] - INSERT 実行時の sqlite3_exec の戻り値が SQLITE_OK であること。
+
+    // Act_4
     sqlite3_stmt *stmt = nullptr;
     int prepare_rc = sqlite3_prepare_v2(db, "SELECT id, name FROM t", -1, &stmt,
                                         nullptr); // [手順] - SELECT 文を sqlite3_prepare_v2 でコンパイルする。
     int step_rc = sqlite3_step(stmt);             // [手順] - sqlite3_step(stmt) で 1 行目を取得する。
 
-    // Assert_3
+    // Assert_4
     ASSERT_EQ(SQLITE_OK, prepare_rc);          // [確認_正常系] - sqlite3_prepare_v2 の戻り値が SQLITE_OK であること。
     ASSERT_EQ(SQLITE_ROW, step_rc);            // [確認_正常系] - sqlite3_step の戻り値が SQLITE_ROW であること。
     EXPECT_EQ(1, sqlite3_column_int(stmt, 0)); // [確認_正常系] - id 列の値が 1 であること。
@@ -57,7 +60,6 @@ TEST_F(sqliteTest, exec_invalid_sql_returns_error)
     char *errmsg = nullptr;
 
     // Pre-Assert
-    ASSERT_NE(nullptr, db); // [Pre-Assert確認_正常系] - db が NULL でないこと (以降の Act 前提)。
 
     // Act
     int rc = sqlite3_exec(db, "SELEKT * FROM nothing", nullptr, nullptr,
