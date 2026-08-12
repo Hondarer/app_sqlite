d85dc92e3e32d34a7d2ecb8773283048465f831f
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-08-12 22:58:47 +0900

    gccの変数追跡サイズ制限に関する設定を追加
    Mock_sqlite3::Mock_sqlite3() が多数の ON_CALL を持つため、GCC の
    -fvar-tracking-assignments が内部サイズ制限を超え、ビルドのたびに
    備考メッセージが出力されていた。mock_com_util と同様の対策を追加する。

--- test/libsrc/mock_sqlite3/makepart.mk
+++ test/libsrc/mock_sqlite3/makepart.mk
@@ -1,3 +1,20 @@
+ifdef PLATFORM_LINUX
+    # Mock_sqlite3::Mock_sqlite3() コンストラクターが多数の ON_CALL を持つため、GCC の
+    # -fvar-tracking-assignments が内部サイズ制限を超え、以下のメッセージが出力されることがある。
+    #
+    # mock_sqlite3.cc:77:1: 備考: 変数追跡サイズ制限が -fvar-tracking-assignments を超過しています。
+    # -fvar-tracking-assignments 無しで再度試みています
+    #
+    # var-tracking-assignments-dom-ops-limit に対応している GCC では、上限値を引き上げて備考を抑制する。
+    # gcc 8 系のように未対応の環境では、-fno-var-tracking-assignments に切り替えて回避する。
+    GCC_VAR_TRACKING_ASSIGNMENTS_DOM_OPS_LIMIT_SUPPORTED := $(shell $(CXX) -Q --help=params 2>/dev/null | grep -q '^[[:space:]]*var-tracking-assignments-dom-ops-limit[[:space:]]' && echo 1)
+    ifneq ($(GCC_VAR_TRACKING_ASSIGNMENTS_DOM_OPS_LIMIT_SUPPORTED),)
+        CXXFLAGS += --param=var-tracking-assignments-dom-ops-limit=400000
+    else
+        CXXFLAGS += -fno-var-tracking-assignments
+    endif
+endif
+
 ifdef PLATFORM_WINDOWS
     # SQLite の DLL import ではなくモックの実シンボルを定義する。
     DEFINES += SQLITE_API=
